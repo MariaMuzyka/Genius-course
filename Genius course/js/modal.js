4d0d5133e01e8b8987637582f8666b0a394cae12
MariaMuzyka <mariamuzyka27@gmail.com>
2024-12-01 19:35:52 +0200

    button event fix

--- Genius course/js/modal.js
+++ Genius course/js/modal.js
@@ -1,8 +1,9 @@
-const modal = document.querySelector('.backdrop');
+const modal = document.querySelector(('.backdrop'));
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+
+const toggleModal = () => modal.classList.toggle('visually-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
